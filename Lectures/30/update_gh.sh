f71c30d454ec49830ab2ac2783e5ee7c42bbf511
Yue Li <liyue@mit.edu>
2019-03-22 11:44:11 -0400

    update

--- Lectures/30/update_gh.sh
+++ Lectures/30/update_gh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm Lecture30_code.ipynb
-rsync -avz ~/Teaching/COMP204_Winter2019/Lectures/30/Lecture30_code.ipynb ./
+rm Lecture30_titanic.ipynb
+rsync -avz ~/Teaching/COMP204_Winter2019/Lectures/30/Lecture30_titanic.ipynb ./
 git add .
 git commit -m 'update'
 git push
