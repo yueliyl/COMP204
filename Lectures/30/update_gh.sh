56fbb697a27e2c9a6682fd4127f5d31a3c544254
Yue Li <liyue@mit.edu>
2019-03-22 11:43:41 -0400

    update

---
+++ Lectures/30/update_gh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm Lecture30_code.ipynb
+rsync -avz ~/Teaching/COMP204_Winter2019/Lectures/30/Lecture30_code.ipynb ./
+git add .
+git commit -m 'update'
+git push
+
---
+++ Lectures/30/update_gh.sh~
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm Lecture29_code.ipynb
+rsync -avz ~/Teaching/COMP204_Winter2019/Lectures/29/Lecture29_code.ipynb ./
+git add .
+git commit -m 'update'
+git push
+
